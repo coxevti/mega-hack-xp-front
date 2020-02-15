1945dee7cb48fe7042158aeceb16ff4c4a1985fe
Valdir Coxev <valcox.russo@gmail.com>
2020-02-15 09:05:33 -0400

    Adding global style and standard route

---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #30c39e, #2dde98);
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  *:focus {
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased !important;
+  }
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
