8a7417fde7ce38b01fa6ded83b341107ccd7b701
Valdir Coxev <valdircoxevdev@gmail.com>
2020-02-15 23:43:14 -0400

    Adding home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Button from '~/components/Button';
+
+import { HomePageElm } from './styles';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomePageElm>
+      <h1>
+        Olá! Não investe por achar que não tem recurso, ou por ser muito
+        complexo, por medo de perder TUDO ?
+      </h1>
+      <p>
+        Nosso orientador te ajudará no conhecimento, acesso, oportunidade de
+        forma digital, capaz de levar a segurança, educação financeira para sabe
+        se relacionar com dinheiro.
+      </p>
+      <Button label="Vamos começar?" navigateTo="/dashboard" />
+    </HomePageElm>
   );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const HomePageElm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  text-align: center;
+  padding: 20px;
+
+  height: 100%;
+
+  h1 {
+    font-size: 3.5em;
+    font-weight: bold;
+  }
+  p {
+    line-height: normal;
+    font-weight: normal;
+    margin-top: 10px;
+    font-size: 1.5em;
+    margin: 30px 0;
+  }
+`;
